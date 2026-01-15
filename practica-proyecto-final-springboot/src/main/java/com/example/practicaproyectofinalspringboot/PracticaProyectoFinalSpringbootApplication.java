9ffee0142cb9c6fde5441a765c0e50475893865f
Darwin Sánchez Vera <dsanchezv@uteq.edu.ec>
2026-01-14 22:08:28 -0500

    Initialize Spring Boot project with JPA and PostgreSQL
    Add initial Spring Boot project structure including Maven wrapper, .gitignore, and configuration files. Set up dependencies for Spring Data JPA, PostgreSQL, and devtools. Include a basic application class and a test to verify context loading.

---
+++ practica-proyecto-final-springboot/src/main/java/com/example/practicaproyectofinalspringboot/PracticaProyectoFinalSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicaproyectofinalspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaProyectoFinalSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticaProyectoFinalSpringbootApplication.class, args);
+    }
+
+}
---
+++ practica-proyecto-final-springboot/src/test/java/com/example/practicaproyectofinalspringboot/PracticaProyectoFinalSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicaproyectofinalspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaProyectoFinalSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
